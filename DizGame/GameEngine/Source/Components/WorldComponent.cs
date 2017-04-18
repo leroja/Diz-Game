edbedef793c37707785911f2ccf0fa561eba8e3e
AllePihl <alexander.pihl.22@hotmail.com>
2017-04-18 10:49:12 +0200

    WorldComponent

---
+++ DizGame/GameEngine/Source/Components/WorldComponent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using GameEngine.Source.Components.Interface;
+
+namespace GameEngine.Source.Components
+{
+    class WorldComponent : IComponent
+    {
+        Matrix _world { get; set; }
+
+        public WorldComponent(Matrix world)
+        {
+            _world = world;
+        }
+        
+    }
+}
