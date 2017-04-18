0184bc87ea3e36eabf61f2feb93394b2b674ddae
Rasmus15 <rasmus_backerhall@hotmail.com>
2017-04-18 14:59:11 +0200

    Added CollisionSystem.

---
+++ DizGame/GameEngine/Source/Systems/CollisionSystem.cs
@@ -0,0 +1,18 @@
+﻿using GameEngine.Source.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Source.Systems
+{
+    class CollisionSystem
+    {
+        public bool CollisionDetection()
+        {
+            ComponentManager.Instance.GetAllEntitiesWithComponentType<>
+            return true;
+        }
+    }
+}
