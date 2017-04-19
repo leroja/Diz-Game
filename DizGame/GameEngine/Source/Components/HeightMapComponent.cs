1f17b064bb5a6ed9f13c1ba6703149a87d76077f
Henrik <S142066@student.hb.se>
2017-04-19 11:05:00 +0200

    A tested heightmapsystem and heightmapobject.

--- DizGame/GameEngine/Source/Components/HeightMapComponent.cs
+++ DizGame/GameEngine/Source/Components/HeightMapComponent.cs
@@ -3,18 +3,29 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace GameEngine.Source.Components
 {
-    class HeightMapComponent : IComponent
+    public class HeightMapComponent : IComponent
     {
 
+        public string terrainMapName { get; set; }
 
         public int terrainWidth { get; set; }
         public int terrainHeight { get; set; }
+
+        public float scaleFactor { get; set; }
 
         public VertexPositionNormalTexture[] vertices { get; set; }
         public VertexBuffer vertexBuffer { get; set; }
 
         public IndexBuffer indexBuffer { get; set; }
         public int[] indices { get; set; }
+
+        //to be set for other systems to get height data from map.
+        //public int[,] heightData { get; set; }
+
+        public HeightMapComponent()
+        {
+
+        }
 
     }
 }
