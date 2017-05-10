5b5ee48de75a25a6d5bbeb571c4fdc17544c06d4
Zzooki <zzoooki@gmail.com>
2017-05-10 15:12:17 +0200

    Added a GameStateManager aswell as a base class for gamestates with some functions that should be implemented for each gamestate. Also added a mainmenu class in the game to start implementing some gamestates. Also changed name of a folder which we recieved feedback for (was called interface) when there was an abstract class in the folder

---
+++ DizGame/DizGame/Source/GameStates/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using GameEngine.Source.Components.Abstract_Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DizGame.Source.GameStates
+{
+    public class MainMenu : GameState
+    {
+        public override List<int> GameStateEntities => throw new NotImplementedException();
+
+        public override void Entered()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Exiting()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Obscuring()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Revealed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
