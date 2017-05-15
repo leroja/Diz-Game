3cda9418b68f90deeb23aab4d34069b3eb864b22
DraganaTod <todorovic.dragana@hotmail.com>
2017-05-15 13:18:55 +0200

    soundsystem added

---
+++ DizGame/GameEngine/Source/Components/SkyBoxComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Source.Components
+{
+    class SkyBoxComponent : IComponent
+    {
+        public Model SkyboxModel { get; set; }
+        public Effect SkyboxEffect { get; set; }
+        public float Size { get; set; } = 295;
+        public TextureCube SkyboxTextureCube { get; set; }
+    }
+}
